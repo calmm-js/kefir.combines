a98016a6d84cd9a8cddbd251ab032ff4432a259f
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-04-01 16:24:10 +0300

    Avoiding `eval`

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
-import * as Kefir from 'kefir'
+import {Observable, constant as C, constantError as E, later} from 'kefir'
 import * as R from 'ramda'
 
-import K, * as C from '../dist/kefir.combines.cjs'
+import {combine, lift, lift1, lift1Shallow} from '../dist/kefir.combines.cjs'
 
 function show(x) {
   switch (typeof x) {
@@ -15,86 +15,82 @@ function show(x) {
 
 const objectConstant = {x: 1}
 
-const testEq = (exprIn, expect) => {
-  const expr = exprIn.replace(/[ \n]+/g, ' ')
-  it(`${expr} => ${show(expect)}`, done => {
-    const actual = eval(`(C, K, Kefir, R, objectConstant) => ${expr}`)(
-      C,
-      K,
-      Kefir,
-      R,
-      objectConstant
-    )
-    const check = actual => {
+const toExpr = f =>
+  f
+    .toString()
+    .replace(/\s+/g, ' ')
+    .replace(/^\s*function\s*\(\s*\)\s*{\s*(return\s*)?/, '')
+    .replace(/\s*;?\s*}\s*$/, '')
+    .replace(/function\s*(\([a-zA-Z0-9, ]*\))\s*/g, '$1 => ')
+    .replace(/\(([^),]+)\) =>/, '$1 =>')
+    .replace(/{\s*return\s*([^{;]+)\s*;\s*}/g, '$1')
+    .replace(/{\s*return\s*([^{;]+)\s*;\s*}/g, '$1')
+    .replace(/\(0, [^.]*[.]([^)]*)\)/g, '$1')
+
+const testEq = (expect, thunk) =>
+  it(`${toExpr(thunk)} => ${show(expect)}`, done => {
+    const actual = thunk()
+    function check(actual) {
       if (!R.equals(actual, expect))
         throw new Error(`Expected: ${show(expect)}, actual: ${show(actual)}`)
       done()
     }
-    if (actual instanceof Kefir.Observable)
+    if (actual instanceof Observable) {
       actual.take(1).observe({value: check, error: check})
-    else check(actual)
+    } else {
+      check(actual)
+    }
   })
-}
 
-describe('K', () => {
-  testEq(`K()`, [])
+describe('combine', () => {
+  testEq([], () => combine())
 
-  testEq(`K("a")`, ['a'])
-  testEq(`K(Kefir.constant("a"))`, ['a'])
+  testEq(['a'], () => combine('a'))
+  testEq(['a'], () => combine(C('a')))
 
-  testEq(`K("a", "b")`, ['a', 'b'])
-  testEq(`K("a", Kefir.constant("b"))`, ['a', 'b'])
-  testEq(`K(Kefir.constant("a"), "b")`, ['a', 'b'])
-  testEq(`K(Kefir.later(10,"a"), Kefir.later(2,"b"))`, ['a', 'b'])
+  testEq(['a', 'b'], () => combine('a', 'b'))
+  testEq(['a', 'b'], () => combine('a', C('b')))
+  testEq(['a', 'b'], () => combine(C('a'), 'b'))
+  testEq(['a', 'b'], () => combine(later(10, 'a'), later(2, 'b')))
 
-  testEq(`K("a", x => x + x)`, 'aa')
-  testEq(`K(Kefir.constant("a"), x => x + x)`, 'aa')
-  testEq(`K(Kefir.later(1,"a"), x => x + x)`, 'aa')
-  testEq(`K(Kefir.constant("a"), Kefir.constant(x => x + x))`, 'aa')
+  testEq('aa', () => combine('a', x => x + x))
+  testEq('aa', () => combine(C('a'), x => x + x))
+  testEq('aa', () => combine(later(1, 'a'), x => x + x))
+  testEq('aa', () => combine(C('a'), C(x => x + x)))
 
-  testEq(
-    `K([1, {y: {z: Kefir.constant("x")}}, Kefir.constant(3)],
-            R.prepend(4))`,
-    [4, 1, {y: {z: 'x'}}, 3]
+  testEq([4, 1, {y: {z: 'x'}}, 3], () =>
+    combine([1, {y: {z: C('x')}}, C(3)], R.prepend(4))
   )
 
-  testEq(`K(Kefir.constantError("e"))`, 'e')
-  testEq(`K(Kefir.constantError("e"), x => x + x)`, 'e')
-  testEq(`K(Kefir.constant("f"), Kefir.constantError("e"))`, 'e')
+  testEq('e', () => combine(E('e')))
+  testEq('e', () => combine(E('e'), x => x + x))
+  testEq('e', () => combine(C('f'), E('e')))
 
-  testEq(
-    `K(objectConstant,
-            Kefir.constant(objectConstant),
-            (o1, o2) => objectConstant === o1
-                     && objectConstant === o2)`,
-    true
+  testEq(true, () =>
+    combine(
+      objectConstant,
+      C(objectConstant),
+      (o1, o2) => objectConstant === o1 && objectConstant === o2
+    )
   )
 })
 
 describe('lift1', () => {
-  testEq(`C.lift1(R.add(1))(2)`, 3)
-  testEq(`C.lift1(R.add(1))(Kefir.constant(2))`, 3)
-  testEq(`C.lift1(R.map(R.add(1)))([1,Kefir.constant(2),3])`, [2, 3, 4])
-  testEq(`C.lift1(R.map(R.add(1)))([Kefir.constant(1),2,Kefir.constant(3)])`, [
-    2,
-    3,
-    4
-  ])
+  testEq(3, () => lift1(R.add(1))(2))
+  testEq(3, () => lift1(R.add(1))(C(2)))
+  testEq([2, 3, 4], () => lift1(R.map(R.add(1)))([1, C(2), 3]))
+  testEq([2, 3, 4], () => lift1(R.map(R.add(1)))([C(1), 2, C(3)]))
 })
 
 describe('lift1Shallow', () => {
-  testEq(`C.lift1Shallow(R.add(1))(2)`, 3)
+  testEq(3, () => lift1Shallow(R.add(1))(2))
 })
 
 describe('lift', () => {
-  testEq(`C.lift(() => 42)()`, 42)
-  testEq(`C.lift(R.add(1))(2)`, 3)
-  testEq(`C.lift(R.add)(2, 3)`, 5)
-  testEq(`C.lift(R.add)(2)(Kefir.constant(3))`, 5)
-  testEq(`C.lift(R.add)(Kefir.constant(3), 2)`, 5)
-  testEq(
-    `C.lift(R.map)(Kefir.constant(R.add(1)),
-                        [Kefir.constant(1),2,Kefir.constant(3)])`,
-    [2, 3, 4]
-  )
+  testEq(42, () => lift(() => 42)())
+  testEq(3, () => lift(R.add(1))(2))
+  testEq(5, () => lift(R.add)(2, 3))
+  testEq(5, () => lift(R.add)(2)(C(3)))
+  testEq(5, () => lift(R.add)(C(3), 2))
+  testEq([2, 3, 4], () => lift(R.map)(C(R.add(1)), [C(1), 2, C(3)]))
 })
